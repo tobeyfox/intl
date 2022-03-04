c9e08bab684c06174fd0df19a8ee2dca11a0a364
Tobias Reimann <info@tobias-reimann.com>
2022-03-04 12:36:07 +0100

    Updates

--- intl.js
+++ intl.js
@@ -1,38 +1,39 @@
-const I = new Proxy((() =>
-{
-    let obj = {        
+const I = new Proxy((() => {
+    let obj = {
         dict: {},
         lang: 'en',
-        get: function(key)
-        {
-            if (this.dict.hasOwnProperty(key))
-            {
+        get: function (key) {
+            if (this.dict.hasOwnProperty(key)) {
                 return this.dict[key][this.lang];
             }
-            else
-            {
+            else {
                 console.log('Missing translation: ' + key);
-                return '***' + key +  '***';
+                return '***' + key + '***';
             }
         }
     };
     return obj;
 })(),
-{
-    get: function(obj, name)
     {
-        return obj.get(name);
-    
-    },
-    set: function(obj, name, value)
-    {
-        obj.dict[name] = value;
-        return true;
-    },
-    setLanguage(lang)
-    {
-        obj.lang = lang;
-    }
-});
+        get: function (obj, name) {
+            if (name === '_lang') {
+                return obj.lang;
+            }
+            else if (name === 'setLanguage')
+            {
+                return (lang) => { obj.lang = lang; };
+            }
+            return obj.get(name);
+
+        },
+        set: function (obj, name, value) {
+            if (name === '_lang') {
+                obj.lang = value;
+            } else {
+                obj.dict[name] = value;
+            }
+            return true;
+        }
+    });
 
 export { I };
