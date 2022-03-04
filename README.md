e2cbc88f431ec0a40e424ff623a7e868e361a68c
Tobias Reimann <info@tobias-reimann.com>
2022-03-04 12:13:38 +0100

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # intl
 Minimalistic internationalization library
+
+## Installation
+
+```
+npm install tobeyfox/intl
+```
+
+## Usage
+
+```
+
+```
---
+++ intl.js
@@ -0,0 +1,34 @@
+const I = new Proxy((() =>
+{
+    let obj = {        
+        dict: {},
+        lang: 'en',
+        get: function(key)
+        {
+            if (this.dict.hasOwnProperty(key))
+            {
+                return this.dict[key][this.lang];
+            }
+            else
+            {
+                console.log('Missing translation: ' + key);
+                return '***' + key +  '***';
+            }
+        }
+    };
+    return obj;
+})(),
+{
+    get: function(obj, name)
+    {
+        return obj.get(name);
+    
+    },
+    set: function(obj, name, value)
+    {
+        obj.dict[name] = value;
+        return true;
+    }
+});
+
+export { I };
